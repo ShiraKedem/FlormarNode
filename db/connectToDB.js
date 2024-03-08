36bff41c27319875e6364a62e1a8162e65684794
ShiraKedem <shirakedem4244@gmail.com>
2024-03-08 16:12:16 +0200

    chang env

--- db/connectToDB.js
+++ db/connectToDB.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectToDB = async () => {
-    try {
-        let connect = await mongoose.connect(process.env.PORT||"mongodb+srv://shirakedem4244:WeVfLXykcKGhyttK@progectnode.j6l7wqm.mongodb.net/")
-        console.log("mongo db connected")
-    }
-    catch (err) {
-        console.log(err);
-        console.log("cannot connect to db");
-        process.exit(1)
-    }
-}
+  try {
+    let connect = await mongoose.connect(
+      process.env.DB_URI ||
+        "mongodb+srv://shirakedem4244:WeVfLXykcKGhyttK@progectnode.j6l7wqm.mongodb.net/"
+    );
+    console.log("mongo db connected");
+  } catch (err) {
+    console.log(err);
+    console.log("cannot connect to db");
+    process.exit(1);
+  }
+};
